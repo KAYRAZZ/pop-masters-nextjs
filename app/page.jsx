3a7d32496fa65c32246640eab2e72428dd44d92e
KAYRAZZ <valentin.chigot@orange.fr>
2024-01-18 12:11:55 +0100

    Add files via upload

--- app/page.jsx
+++ app/page.jsx
@@ -15,10 +15,16 @@ const Dashboard = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("/api/getLastFigurines");
+        const response = await fetch("/api/getLastFigurines", {
+          method: "GET",
+          headers: {
+            "Authorization": "Bearer " + process.env.TOKEN_VERCEL
+          }
+        });
         const data = await response.json();
 
         setRecentlyAdded(data.recentlyAddedResult);
+        console.log(recentlyAdded);
 
       } catch (error) {
         console.error('Erreur lors de la récupération des données :', error);
@@ -35,7 +41,7 @@ const Dashboard = () => {
       <div className="container" id="bloc-recentlyAdded">
         <span id="recentlyAdded">Les 5 dernières POP ajoutées à la collection</span>
         <div id="little-figurines">
-          {recentlyAdded && recentlyAdded.length > 1 && recentlyAdded.map((item, index) => (
+          {recentlyAdded && recentlyAdded.map((item, index) => (
             <a key={index} className="little-figurine" href={`/figurine/${item.figurine_id}`}>
               <img src={item.figurine_image} />
               <div>
