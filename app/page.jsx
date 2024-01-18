b034d35d061bc53186a170664fda19a743cb22e8
KAYRAZZ <valentin.chigot@orange.fr>
2024-01-18 12:29:13 +0100

    Add files via upload

--- app/page.jsx
+++ app/page.jsx
@@ -1,59 +1,36 @@
 "use client"
-import ProtectedPages from "@/lib/protectedPages"
+import ProtectedPages from "@/lib/protectedPages";
 import { useEffect, useState } from "react";
-import ParcelTracking from "./components/ParcelTracking";
-import { useSession } from "next-auth/react";
 
-const Dashboard = () => {
-  ProtectedPages();
+const MyCollection = () => {
+    ProtectedPages();
 
-  const { data: session } = useSession()
-  // console.log(session);
+    const [collection, setCollection] = useState([]);
 
-  const [recentlyAdded, setRecentlyAdded] = useState([]);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch("/api/getCollectionUser");
+                const data = await response.json();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("/api/getLastFigurines", {
-          method: "GET",
-          headers: {
-            "Authorization": "Bearer " + process.env.TOKEN_VERCEL
-          }
-        });
-        const data = await response.json();
+                setCollection(data.collection);
 
-        setRecentlyAdded(data.recentlyAddedResult);
-        console.log(recentlyAdded);
+            } catch (error) {
+                console.error('Erreur lors de la récupération des données :', error);
+            }
+        }
+        fetchData();
+    }, [])
 
-      } catch (error) {
-        console.error('Erreur lors de la récupération des données :', error);
-      }
-    };
 
-    fetchData();
-  }, []);
-
-  return (
-    <section id="dashboard">
-      <ParcelTracking />
-
-      <div className="container" id="bloc-recentlyAdded">
-        <span id="recentlyAdded">Les 5 dernières POP ajoutées à la collection</span>
-        <div id="little-figurines">
-          {recentlyAdded && recentlyAdded.map((item, index) => (
-            <a key={index} className="little-figurine" href={`/figurine/${item.figurine_id}`}>
-              <img src={item.figurine_image} />
-              <div>
-                <span>{item.figurine_name}</span>
-              </div>
-            </a>
-          ))}
-        </div>
-      </div>
-
-    </section>
-  )
+    return (
+        <section id='collections'>
+            {collection && collection.map((item, index) => (
+                <a key={index} className='collection' href={"/collections/" + item.collection_name}>
+                    {item.collection_name}
+                </a>
+            ))}
+        </section>
+    )
 }
-
-export default Dashboard;
+export default MyCollection;
