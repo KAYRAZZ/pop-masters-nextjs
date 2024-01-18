0b0537e509363e2005abf9feeec138d0f82026a7
KAYRAZZ <valentin.chigot@orange.fr>
2024-01-18 12:04:33 +0100

    Add files via upload

--- pages/api/getLastFigurines.js
+++ pages/api/getLastFigurines.js
@@ -4,7 +4,7 @@ import { getToken } from "next-auth/jwt";
 export default async function handler(req, res) {
     const token = await getToken({ req });
     const user_id = token.sub;
-
+    console.log("api atteint");
     try {
         let getRecentlyAddedResult = await prisma.$queryRaw`select cd.figurine_id, cd.figurine_name, c.collection_name, cd.figurine_image from collection c join collectionsDatas cd on c.figurine_id=cd.figurine_id where user_id=${user_id} order by date_figurine_added DESC limit 5;`;
 
